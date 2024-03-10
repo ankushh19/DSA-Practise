af0e9e047edcf25b156bd030079bfc2ba3d4ed74
ankushh19 <ak9108824@gmail.com>
2024-03-10 13:20:15 +0530

    new questions

---
+++ index.js
@@ -0,0 +1,30 @@
+// let arr = [46, 12, 78, 45, 23, 6, 67, 43, 65, 45];
+
+// function sortedArray(arr) {
+//   let temp = 0;
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > arr[temp]) {
+//       arr[temp] = arr[i + 1];
+//       arr[i + 1] = temp;
+//     }
+//   }
+//   return arr;
+// }
+
+// console.log(sortedArray(arr));
+
+// let nums = [2, 7, 11, 15];
+// target = 9
+// function sum(nums, target) {
+//   let sum = [];
+//   for (let i = 0; i < nums.length; i++) {
+//     for (let j = i + 1; j < nums.length; j++) {
+//       if (nums[i] + nums[j] === target) {
+//         sum.push(i, j);
+//       }
+//     }
+//   }
+//   return sum;
+// }
+// console.log(sum(nums, 9));
