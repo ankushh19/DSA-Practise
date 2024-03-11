8c11d99c9fd667c3a6609bcc5038987802b37950
ankushh19 <ak9108824@gmail.com>
2024-03-11 23:10:04 +0530

    practising dsa

--- index.js
+++ index.js
@@ -1,21 +1,5 @@
-// let arr = [46, 12, 78, 45, 23, 6, 67, 43, 65, 45];
-
-// function sortedArray(arr) {
-//   let temp = 0;
-
-//   for (let i = 0; i < arr.length; i++) {
-//     if (arr[i] > arr[temp]) {
-//       arr[temp] = arr[i + 1];
-//       arr[i + 1] = temp;
-//     }
-//   }
-//   return arr;
-// }
-
-// console.log(sortedArray(arr));
-
 // let nums = [2, 7, 11, 15];
-// target = 9
+// target = 9;
 // function sum(nums, target) {
 //   let sum = [];
 //   for (let i = 0; i < nums.length; i++) {
@@ -28,3 +12,58 @@
 //   return sum;
 // }
 // console.log(sum(nums, 9));
+
+// // Binary Search
+// let arr1 = [10, 20, 30, 40, 50, 60, 70, 80];
+
+// function binarySearch(arr, target) {
+//   let si = 0;
+//   let ei = arr.length - 1;
+//   while (si <= ei) {
+//     let mid = Math.floor((si + ei) / 2);
+
+//     if (arr[mid] > target) {
+//       ei = mid - 1;
+//     } else if (arr[mid] === target) {
+//       return mid;
+//     } else if (arr[mid] < target) {
+//       si = mid + 1;
+//     }
+//   }
+//   return -1;
+// }
+// console.log(binarySearch(arr1, 20));
+
+// Selection Sort
+// let arr = [2, -8, 10, -1, 50, 7];
+// for (let i = 0; i < arr.length; i++) {
+//   let min = i;
+//   for (let j = i + 1; j < arr.length; j++) {
+//     if (arr[j] < arr[min]) {
+//       min = j;
+//     }
+//   }
+//   let temp = arr[min];
+//   arr[min] = arr[i];
+//   arr[i] = temp;
+// }
+// console.log(arr);
+
+//Bubble Sort
+
+// Using Bubble Sort
+let arr = [12, 32, 44, 2, 13, 41, 21];
+
+function sort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] > arr[j]) {
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return arr;
+}
+console.log(sort(arr));
