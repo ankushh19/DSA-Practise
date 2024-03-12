e242d39e80e73dca1ddb1cd1c9ac8d866717c48a
ankushh19 <ak9108824@gmail.com>
2024-03-12 23:40:08 +0530

    practice

--- index.js
+++ index.js
@@ -68,20 +68,92 @@
 // }
 // console.log(sort(arr));
 
-let arr = [10, -2, 0, -1, 7];
+// let arr = [10, -2, 0, -1, 7];
 
-// using two pointers approach
-function reverse(arr) {
-  let i = 0;
-  let j = arr.length - 1;
+// // using two pointers approach
+// function reverse(arr) {
+//   let i = 0;
+//   let j = arr.length - 1;
 
-  while (i <= j) {
-    let temp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = temp;
-    i++;
-    j--;
+//   while (i <= j) {
+//     let temp = arr[i];
+//     arr[i] = arr[j];
+//     arr[j] = temp;
+//     i++;
+//     j--;
+//   }
+//   return arr;
+// }
+// console.log(reverse(arr));
+
+// Kaden's Algorithm to find the maximum sum of a contiguous subarray within the array
+let arr2 = [2, -3, 1, -5, 8];
+
+let i = 0;
+let sum = 0;
+let max = Number.NEGATIVE_INFINITY;
+while (i < arr2.length) {
+  sum = sum + arr2[i];
+  max = Math.max(sum, max);
+
+  if (sum < 0) {
+    sum = 0;
   }
-  return arr;
+  i++;
 }
-console.log(reverse(arr));
+
+console.log(max);
+
+// Check Palindrome using two pointer approach
+
+// let str = "madam";
+// function palindrome(str) {
+//   let i = 0;
+//   let j = str.length - 1;
+
+//   while (i <= j) {
+//     if (str.charAt(i) === str.charAt(j)) {
+//       i++;
+//       j--;
+//     } else {
+//       return "Not Palindrome";
+//       break;
+//     }
+//   }
+//   return "Is Palindrome";
+// }
+
+// console.log(palindrome(str));
+
+// checking Palindrome with checking substrings
+// let str = "nitin";
+// let arr = [];
+// function palindrome(str) {
+//   let i = 0;
+//   let j = str.length - 1;
+
+//   while (i <= j) {
+//     if (str.charAt(i) === str.charAt(j)) {
+//       i++;
+//       j--;
+//     } else {
+//       return false;
+//       break;
+//     }
+//   }
+//   return true;
+// }
+
+// function palindromeSubstring(str) {
+//   for (let i = 0; i < str.length; i++) {
+//     for (let j = i + 1; j <= str.length; j++) {
+//       let ch = str.substring(i, j);
+//       if (palindrome(ch)) {
+//         arr.push(ch);
+//       }
+//     }
+//   }
+//   return arr;
+// }
+
+// console.log(palindromeSubstring(str));
