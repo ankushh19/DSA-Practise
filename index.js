6ef0131771abd9cb0a57b5160a27bd2b0504e841
ankushh19 <ak9108824@gmail.com>
2024-03-24 18:48:10 +0530

    practise

--- index.js
+++ index.js
@@ -325,25 +325,138 @@
 
 // Examples:
 
-console.log(isSubsequence("hello", "hello world")); // true
-console.log(isSubsequence("sing", "sting")); // true
-console.log(isSubsequence("abc", "abracadabra")); // true
-console.log(isSubsequence("abc", "acb")); // false (order matters)
+// console.log(isSubsequence("hello", "hello world")); // true
+// console.log(isSubsequence("sing", "sting")); // true
+// console.log(isSubsequence("abc", "abracadabra")); // true
+// console.log(isSubsequence("abc", "acb")); // false (order matters)
 
-function isSubsequence(str1, str2) {
-  let i = 0;
-  let j = 0;
-  if (!str1) {
-    return true;
-  }
-  while (j < str2.length) {
-    if (str2[j] === str1[i]) {
-      i++;
+// Iterative
+// function isSubsequence(str1, str2) {
+//   let i = 0;
+//   let j = 0;
+//   if (!str1) {
+//     return true;
+//   }
+//   while (j < str2.length) {
+//     if (str2[j] === str1[i]) {
+//       i++;
+//     }
+//     if (i === str1.length) {
+//       return true;
+//     }
+//     j++;
+//   }
+//   return false;
+// }
+
+// Recursive
+// function isSubsequence(str1, str2) {
+//   if (str1.length === 0) {
+//     return true;
+//   }
+//   if (str2.length === 0) {
+//     return false;
+//   }
+//   if (str2[0] === str1[0]) {
+//     return isSubsequence(str1.slice(1), str2.slice(1));
+//   }
+//   return isSubsequence(str1, str2.slice(1));
+// }
+
+// Sliding Window - maxSubarraySum
+// Given an array of integers and a number, write a function called maxSubarraySum, which finds the maximum sum of a subarray with the length of the number passed to the function.
+
+// Note that a subarray must consist of consecutive elements from the original array. In the first example below, [100, 200, 300] is a subarray of the original array, but [100, 300] is not.
+
+// Constraints:
+
+// Time Complexity - O(N)
+
+// Space Complexity - O(1)
+
+// console.log(maxSubarraySum([100, 200, 300, 400], 2)); // 700
+// console.log(maxSubarraySum([1, 4, 2, 10, 23, 3, 1, 0, 20], 4)); // 39
+// console.log(maxSubarraySum([-3, 4, 0, -2, 6, -1], 2)); // 5
+// console.log(maxSubarraySum([3, -2, 7, -4, 1, -1, 4, -2, 1], 2)); // 5
+// console.log(maxSubarraySum([2, 3], 3)); // null
+
+// function maxSubarraySum(arr, num) {
+//   if (arr.length < num) {
+//     return null;
+//   }
+//   let total = 0;
+//   for (let i = 0; i < num; i++) {
+//     total += arr[i];
+//   }
+//   let currentTotal = total;
+//   for (let i = num; i < arr.length; i++) {
+//     currentTotal += arr[i] - arr[i - num];
+//     total = Math.max(total, currentTotal);
+//   }
+//   return total;
+// }
+
+// Sliding Window - minSubArrayLen
+// Write a function called minSubArrayLen which accepts two parameters - an array of positive integers and a positive integer.
+
+// This function should return the minimal length of a contiguous subarray of which the sum is greater than or equal to the integer passed to the function. If there isn't one, return 0 instead.
+// Examples:
+
+// console.log(minSubArrayLen([2, 3, 1, 2, 4, 3], 7)); // 2 -> because [4,3] is the smallest subarray
+// minSubArrayLen([2, 1, 6, 5, 4], 9); // 2 -> because [5,4] is the smallest subarray
+// minSubArrayLen([3, 1, 7, 11, 2, 9, 8, 21, 62, 33, 19], 52); // 1 -> because [62] is greater than 52
+// minSubArrayLen([1, 4, 16, 22, 5, 7, 8, 9, 10], 39); // 3
+
+// function minSubArrayLen(nums, sum) {
+//   let total = 0;
+//   let start = 0;
+//   let end = 0;
+//   let minLen = Infinity;
+//   while (start < nums.length) {
+//     // if current window doesn't add up to the given sum then move the window to right
+//     if (total < sum && end < nums.length) {
+//       total += nums[end];
+//       end++;
+//     }
+//     // if current window adds up to at least the sum given then we can shrink the window
+//     else if (total >= sum) {
+//       minLen = Math.min(minLen, end - start);
+//       total -= nums[start];
+//       start++;
+//     }
+//     // current total less than required total but we reach the end, need this or else we'll be in an infinte loop
+//     else {
+//       break;
+//     }
+//   }
+//   return minLen === Infinity ? 0 : minLen;
+// }
+
+// Sliding Window - findLongestSubstring
+// Write a function called findLongestSubstring, which accepts a string and returns the length of the longest substring with all distinct characters.
+
+// Time Complexity - O(n)
+
+console.log(findLongestSubstring("")); // 0
+console.log(findLongestSubstring("rithmschool")); // 7
+findLongestSubstring("thisisawesome"); // 6
+findLongestSubstring("thecatinthehat"); // 7
+findLongestSubstring("bbbbbb"); // 1
+findLongestSubstring("longestsubstring"); // 8
+findLongestSubstring("thisishowwedoit"); // 6
+
+function findLongestSubstring(str) {
+  let longest = 0;
+  let seen = {};
+  let start = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (seen[char]) {
+      start = Math.max(start, seen[char]);
     }
-    if (i === str1.length) {
-      return true;
-    }
-    j++;
+    longest = Math.max(longest, i - start + 1);
+    seen[char] = i + 1;
   }
-  return false;
+  return longest;
 }
